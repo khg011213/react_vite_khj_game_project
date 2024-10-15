6d19e45ed9cebbb0435b825ceac2a25ed2822dd6
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-10-15 11:16:21 +0900

    게시글 삭제 구현

--- src/services/BoardService.jsx
+++ src/services/BoardService.jsx
@@ -4,6 +4,11 @@ const getPagingList = (path = "/boards/list", search = "") => {
   return http.get(path + search);
 };
 
+const remove = (id) => {
+  return http.delete(`boards/${id}`);
+};
+
 export default {
   getPagingList,
+  remove,
 };
