f026fc7d2e27d21f524f0e7045d7a906e9969364
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-10-15 10:19:19 +0900

    서비스 지정

---
+++ src/components/pages/BoardListPage.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import boardService from "../../services/BoardService";
+
+const BoardListPage = () => {
+  const [boards, setBoards] = useState([]);
+
+  // 컴포턴트가 처음 랜더링된후 실행
+  useEffect(() => {
+    console.log("use Effectiv 실행");
+    initBoards();
+  }, []);
+
+  const initBoards = () => {
+    boardService.getPagingList().then((response) => {
+      console.log(response); //성공했을때
+    });
+  };
+
+  return <div></div>;
+};
+
+export default BoardListPage;
